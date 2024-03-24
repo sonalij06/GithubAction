ecd56f9576f614c7146bee08db33ae195fa2146a
sonali <74618887+sonalij06@users.noreply.github.com>
2024-03-24 22:29:15 +0530

    Update commit.py

--- scripts/commit.py
+++ scripts/commit.py
@@ -2,7 +2,7 @@
 
 def main():
     # Ask the user for their name
-    name = input("What's your name? ")
+    name = "sonali"
     
     # Greet the user
     print("Hello, " + name + "! Welcome to the world of Python.")
