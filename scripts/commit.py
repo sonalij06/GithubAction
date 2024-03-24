0220d50b0bd4c8a552dee0f435465620889ccaa7
SonaliJain <sonali.jain.cs.2018@miet.ac.in>
2024-03-24 22:16:43 +0530

    testing script

---
+++ scripts/commit.py
@@ -0,0 +1,11 @@
+# Simple Python script for greeting the user
+
+def main():
+    # Ask the user for their name
+    name = input("What's your name? ")
+    
+    # Greet the user
+    print("Hello, " + name + "! Welcome to the world of Python.")
+
+if __name__ == "__main__":
+    main()
